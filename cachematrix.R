7f60485513cde3f88f29ccdd6e0625c3f0f3ad32
tom <tom@phpfunk.com>
2017-01-18 14:15:43 -0800

    First commit inversion-caching matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## Implement matrix capable of caching its own inversion.
+##
+## Given a matrix returns a list object whose elements are functions
+## - set: store original matrix object
+## - get: get the original matrix object
+## - setInversion: store inversion of matrix into cache
+## - getInversion: retrieve cached inverted matrix, if available or NULL
+## 
+## For example:
+##   cm <- makeCacheMatrix(matrix(1,4,2,2))
+##
+## Call methods of cm object, for example
+##   cachedInversion <- cm$getInversion()
 makeCacheMatrix <- function(x = matrix()) {
+    # our cached inverted matrix, initially NULL
+    cachedInversion <- NULL
+    # makes matrix y into a matrix capable of caching its inversion
+    set <- function(y) {
+        x <<- y
+        cachedInversion <<- NULL
+    }
+    # get our inversion-caching matrix
+    get <-function() {
+        x
+    }
+    # store the inversion of a matrix in the cache
+    setInversion <-function(an_inversion) {
+        cachedInversion <<- an_inversion
+    }
 
+    # return cached inverted matrix
+    getInversion <- function() {
+        cachedInversion
+    }
+
+    # a list where elements are functions
+    # useage:
+    #   foo <- makeCacheMatrix(x)
+    #   invertedMatrix = foo$getInversion()
+    list(set = set, get = get, setInversion = setInversion, getInversion = getInversion)
 }
 
-
-## Write a short comment describing this function
-
+## x is a cachingMatrix obtained by calling makeCacheMatrix()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## check to see if x has inversion in it's cache
+        invertedMatrix = x$getInversion()
+        if (!is.null(invertedMatrix)) {
+            # it's in the cache, celebrate!
+            message("Cache hit")
+            return(invertedMatrix)
+        }
+
+        # get the uncaching matrix so we can call solve() using it as an argument
+        m <- x$get()
+        inversion = solve(m)
+        # store result in the cache
+        x$setInversion(inversion)
+
+        # return the inversion of the matrix
+        inversion
 }
